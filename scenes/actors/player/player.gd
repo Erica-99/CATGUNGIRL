c868074a28be4f49e5131510a0a395a8023a1af3
Erica-99 <turbocake17@gmail.com>
2026-03-20 16:10:42 +1100

    Add mantle checker

--- scenes/actors/player/player.gd
+++ scenes/actors/player/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody3D
 
+signal facing_changed(new_facing: float)
+
 const SPEED: float = 15.0
 const CROUCH_SPEED: float = 10.0
 const JUMP_VELOCITY: float = 25
 
+var facing: float
+
 @export var movement_state_machine: StateMachine
+
+@export_group("Movement Dependencies")
 @export var input_component: InputComponent
+@export var mantle_detector: Node3D
+@export var feet_point: Marker3D
 
 var blackboard: Dictionary
 
@@ -15,6 +23,17 @@ func _ready() -> void:
 	"move_speed" : SPEED,
 	"crouch_speed": CROUCH_SPEED,
 	"input_component": input_component,
+	"mantle_detector": mantle_detector,
+	"feet_point": feet_point,
 	}
 	
 	movement_state_machine.init(blackboard)
+
+func _process(delta: float) -> void:
+	var current_state = input_component.get_input_state()
+	
+	if current_state["movement"] != 0:
+		var new_direction = sign(current_state["movement"])
+		if new_direction != facing:
+			facing = new_direction
+			facing_changed.emit(facing)
