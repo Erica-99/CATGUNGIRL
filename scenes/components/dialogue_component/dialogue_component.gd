da0cd3461014d5dfe27882c151f4f7d63433a990
Emily Hocking <104554365@student.swin.edu.au>
2026-04-09 14:36:30 +1000

    Code comments and handling for dialogue spam
    Yay its done for now! Signals haven't been set up yet, but this can be done at a later stage when other components are finalised.

--- scenes/components/dialogue_component/dialogue_component.gd
+++ scenes/components/dialogue_component/dialogue_component.gd
@@ -1,22 +1,28 @@
 extends Node
 
+# references
 @onready var timer: Timer = $"../Timer"
 @onready var mesh_instance_3d: Sprite3D = $"../.."
 
-@export var min_dialogue_elapsed_time: float = 1.0
-@export var max_dialogue_elapsed_time: float = 1.0
+# export vars
+	# the elapsed time here will be removed to instead favor signals perchance
+	# TODO: at a later stage
+@export var min_dialogue_elapsed_time: float = 2.0
+@export var max_dialogue_elapsed_time: float = 10.0
 @export var can_speak: bool = true
 @export var debug_mode: bool = false
 
 # set variables to change on initialisation
 	# entity can be: player, gun, enemy (including enemy types ie: convict, trunk, scrub and boss)
 	# to be loaded via getting parent details
+	# TODO: implement this when enemies have been set up
 var entity = ""
 var raw_dialogue = {}
 var dialogue_bubble_prefab = preload("res://scenes/dialogue_bubble/dialogue_bubble.tscn")
 
 
 # TEMP NEEDS TO BE LINKED WITH OTHER SECTIONS OF CODE:
+# TODO: link with other sections when I've got a good idea of what can be linked to!
 var player_conditions = {
 	"insanity": 50
 }
@@ -26,50 +32,63 @@ func _ready() -> void:
 	randomize()
 	# here you would get the parent node's ID but I'm not sure if we will do it this way so I'll hardcode for now
 	#entity = get_parent().{insert variable which identified parent like ID or name IDK}
+	# TODO: do at a later stage
 	entity = "player"
+	# load dialogue and begin timer
 	raw_dialogue = DialogueProcessor._load_file(CustomResourceLoader.dialogue_path + entity + ".json")
 	if can_speak:
 		timer.start(randf_range(min_dialogue_elapsed_time, max_dialogue_elapsed_time))
 	
+	# this debug mode is only for if u want to see how the calls work
+	# for like implementing dialogue calls elsewhere
 	if debug_mode:
 		_debug_tests_for_linking()
 
 func _process(delta: float) -> void:
 	pass
 
-	
-
-
+# currently all the creation code is just in this timeout method
+# once i do signals, i'll move the code into a more general method
+# for now this is fine though
 func _on_timer_timeout() -> void:
 	if can_speak:
+		# get dialogue and initialise bubble
 		var dialogue = DialogueProcessor._get_runtime_dialogue(raw_dialogue, trigger, player_conditions)
 		var bubble = dialogue_bubble_prefab.instantiate()
 		add_child(bubble)
-		bubble.owner = self
 		bubble._set_text(dialogue)
+		# make component listen to the child transparency calls
 		bubble.is_transparent.connect(_remove_bubble)
+		# set initial position
 		_update_bubble_positions()
 		
+		# restart timer
 		timer.start(randf_range(min_dialogue_elapsed_time, max_dialogue_elapsed_time))
 
+# remove bubble once transparency is complete
+# ive added this to run in this component rather than the child because I use this as my trigger
+# to update all dialogue bubble positions (rather than the inefficiencies of using process())
 func _remove_bubble(child):
 	child.queue_free()
 	_update_bubble_positions()
 
-# WHY THE FUCK DOES THIS MAKE A PARTICLE ACCELERATOR
-# IT WAS THE FUCKING INDEX ARRGGGGG
-# IF ANYONE TOUCHES THIS SHIT ILL RIP OFF BOTH YOUUR ARMS
+
+# notably this section, if you mess with the indexes and be an idiot (like me) you can make the
+# dialogue appear and move similar to how a particle accelerator operates
+# just dont touch this code lmfao
 func _update_bubble_positions():
 	var children = get_children()
 	if children.size() > 1:
 		var index = 0
 		for child in children:
-			child._set_position_off_index((children.size() - 1) - index)
+			child._update_off_index((children.size() - 1) - index)
 			index += 1
 	else:
-		children[0]._set_position_off_index(0)
+		children[0]._update_off_index()
+		
 
 # chucking this stuff here so its out of the way lol
+# these are just the unit tests for dialogue loading
 func _debug_tests_for_linking() -> void:
 	print("TESTING RUNTIME DIALOGUE:")
 	print("TEST 1: Print random dialogue (using requirement bypass)")
