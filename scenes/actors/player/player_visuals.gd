fa2c37cbf1880b483e61c8cd4a258f4a345018e2
Wacch0wski <162528349+Wacch0wski@users.noreply.github.com>
2026-04-22 15:14:26 +1000

    Player Animation Tweaks
    - Gun hand now tracks laser sight more accurately regardless of torso direction.
    - Further tweaks to animation player to keep sprite positions consistent between changes

--- scenes/actors/player/player_visuals.gd
+++ scenes/actors/player/player_visuals.gd
@@ -4,22 +4,22 @@ extends Node3D
 
 @onready var legs_sprite = $ROOT_P/BODY_P/LEG_P/LEGS_SPRITE
 @onready var torso_sprite = $ROOT_P/BODY_P/TORSO_P/TORSO_SPRITE
+@onready var hand_sprite = $ROOT_P/GUN_P/GUN_AIM/GUN_SPRITE
 
 @onready var torso_pivot = $ROOT_P/BODY_P/TORSO_P
 @onready var leg_pivot = $ROOT_P/BODY_P/LEG_P
 
-@onready var gun_pivot = $ROOT_P/BODY_P/TORSO_P/GUN_P/GUN_AIM
+@onready var hand_pivot = $ROOT_P/GUN_P
+@onready var gun_pivot = $ROOT_P/GUN_P/GUN_AIM
 @onready var gun_component = $"../GunComponent"
 
 @onready var Playeroot = $".."
 var current_action 
 
+
 func _process(delta: float) -> void:
-	
-	gun_pivot.rotation.z = gun_component.rotation.z
-	if gun_pivot.rotation.z >= 90 or gun_pivot.rotation.z <= -90:
-		pass
 		
+	gun_pivot.rotation.z = gun_component.rotation.z
 	if current_action == 'playercrouch':
 		if Input.is_action_pressed("move_left") == true or Input.is_action_pressed("move_right") == true:
 			spriteanimator.play("Walk_Crouching")
@@ -49,10 +49,15 @@ func _on_movement_state_machine_state_changed(_prev: String, new: String) -> voi
 
 
 func _on_player_facing_changed(new_facing: float) -> void:
+	print(new_facing)
 	if new_facing == -1.0:
 		leg_pivot.scale.x = -1
 		torso_pivot.scale.x = -1
+		gun_pivot.scale.x = 1
+		hand_sprite.flip_v = true
 	else:
 		leg_pivot.scale.x = 1
 		torso_pivot.scale.x = 1
+		gun_pivot.scale.x = 1
+		hand_sprite.flip_v = false
 	pass # Replace with function body.
