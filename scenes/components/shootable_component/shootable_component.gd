926110cf361740716443276d916e787269300654
Emily Hocking <104554365@student.swin.edu.au>
2026-04-22 23:41:42 +1000

    Fixed major problems with collision generation
    Ongoing problems with inner collision and bullets being deleted before they can register damage on a shootable component (only occurs when a 'shootable' object is thin). To be fixed at a later date.
    
    Tested on BarrelGoo and BoxElectrical objects.

--- scenes/components/shootable_component/shootable_component.gd
+++ scenes/components/shootable_component/shootable_component.gd
@@ -1,52 +1,73 @@
 extends Node
+# references
 @onready var hurtbox_component: Area3D = $HurtboxComponent
 @onready var hurtbox_collision: CollisionShape3D = $HurtboxComponent/HurtboxCollision
-@onready var internal_collision: CollisionShape3D = $InternalArea/InternalCollision
-
 @onready var team_component: Node = $TeamComponent
 @onready var health_component: Node = $HealthComponent
 
+# variables
 var parent_reference: StaticBody3D
 var parent_animation_player: AnimationPlayer
+var is_collision_generated: bool = false
 
-const internal_collision_scale: float = 0.9
+# constants
+const internal_collision_scale: float = 0.7
 
 func _ready() -> void:
 	parent_reference = get_parent()
-	# res://art/3D_Env/3D_Assets/BarrelGoo.tscn::ConvexPolygonShape3D_x15ns
-	
-	var parent_collision_children = parent_reference.find_children("*", "MeshInstance3D", false)
-	if parent_collision_children.size() > 0:
-		parent_collision_children[0].create_trimesh_collision()
-		#var collision_shape = CollisionShape3D.new()
-		#collision_shape.shape = mesh
-		#print(mesh)
-		#parent_collision_children[0].add_child(collision_shape)
 		
-		
-		hurtbox_collision.shape = parent_collision_children[0].mesh.create_trimesh_shape()
-		#internal_collision.shape = mesh
-		#internal_collision.scale = Vector3(internal_collision_scale,internal_collision_scale, internal_collision_scale)
-		#print(internal_collision.scale)
-	
-	#var parent_collision_children = parent_reference.find_children("*", "CollisionShape3D", false)
-	#if parent_collision_children.size() > 0:
-	#	collision_shape_3d.shape = parent_collision_children[0].shape
-		
+	# set animation player and link signals if animations exist
 	var parent_animation_children = parent_reference.find_children("*", "AnimationPlayer", false)
 	if parent_animation_children.size() > 0:
 		parent_animation_player = parent_animation_children[0]
 		parent_animation_player.animation_finished.connect(_on_animation_finished)
 
-func _process(delta: float) -> void:
-	pass
-
-func _on_health_component_killed(killing_blow: DamageHealInstance, health_before_death: Variant) -> void:
-	parent_animation_player.play("on_destroy")
+func _generate_collision():
+	parent_reference = get_parent()
 	
-func _on_health_component_health_changed(old_health: float, new_health: float, damage_or_heal_instance: DamageHealInstance) -> void:
-	parent_animation_player.play("on_damaged")
+	# this code sucks so bad and entirely breaks inheritance
+	# fix it when i have time
+	var parent_collision_children = parent_reference.find_children("*", "MeshInstance3D", false)
+	if parent_collision_children.size() > 0:
+		# get MeshInstance3D from parent
+		var mesh = parent_collision_children[0]
+		
+		# generate physical collision instance
+		var collision_shape = CollisionShape3D.new()
+		collision_shape.shape = mesh.mesh.create_trimesh_shape()
+		# set to a smaller scale - means the player may clip slightly but bullets will register
+		collision_shape.scale = Vector3(internal_collision_scale,internal_collision_scale, internal_collision_scale)
+		
+		parent_reference.add_child(collision_shape)
+		
+		# assign hurtbox collision shape (this is the external layer of the collision box)
+		hurtbox_collision.shape = parent_collision_children[0].mesh.create_trimesh_shape()
 
+
+# check whether collision loaded - if not load collision
+# this has been put into process because ready wasnt ready (false advertising fr)
+func _process(delta: float) -> void:
+	if not is_collision_generated:
+		_generate_collision()
+		is_collision_generated = true
+
+# play animation on killed
+func _on_health_component_killed(killing_blow: DamageHealInstance, health_before_death: Variant) -> void:
+	if parent_animation_player != null:
+		if parent_animation_player.has_animation("on_destroy"):
+			parent_animation_player.play("on_destroy")
+		else:
+			_on_animation_finished("")
+	else:
+		_on_animation_finished("")
+	
+# play animation on hurt
+func _on_health_component_health_changed(old_health: float, new_health: float, damage_or_heal_instance: DamageHealInstance) -> void:
+	if parent_animation_player != null:
+		if parent_animation_player.has_animation("on_damaged"):
+			parent_animation_player.play("on_damaged")
+
+# if dead, delete self
 func _on_animation_finished(animation_name: String):
-	if animation_name == "on_destroy":
+	if animation_name == "" || animation_name == "on_destroy":
 		get_parent().queue_free()
