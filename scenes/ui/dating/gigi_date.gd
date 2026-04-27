9395461b29a86958893035d78ac8c38c33896d8b
Emily Hocking <104554365@student.swin.edu.au>
2026-04-27 22:44:00 +1000

    Commented GiGiDate
    Comments added everywhere in code
    
    Variables renamed to follow godot snake_case standard

--- scenes/ui/dating/gigi_date.gd
+++ scenes/ui/dating/gigi_date.gd
@@ -1,18 +1,32 @@
 extends Control
 
-var dating_active: bool
-var date_id: int = 0
-var current_dating_scene = []
-var dating_dialogue = {}
-var requires_option_selection: bool = false
-
-@onready var buttonContainer = $ButtonContainer
-@onready var gigiImage = $gigi_pose
+# references
+@onready var button_container = $ButtonContainer
+@onready var gigi_image = $gigi_pose
 @onready var dialogue = $date_txt_bgd/date_txt
 
-func _ready() -> void:
-	visible = false
+# determines whether dating screen should be visible or not
+var dating_active: bool = false
 
+# determines if button must be clicked to proceed
+var requires_option_selection: bool = false
+
+# holds reference to current dating progression (each index is one whole dating scene)
+var date_id: int = 0
+
+# current whole dating scene
+var current_dating_scene: Array = []
+
+# current dialogue section of scene
+var dating_dialogue: Dictionary = {}
+
+
+# ready unused (values which were initialised here have been moved elsewhere)
+func _ready() -> void:
+	pass
+
+# handle date show event
+# TODO: move to player input component as an input state?
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("date_show"):
 		if dating_active:
@@ -21,6 +35,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		else:
 			_date_start()
 
+# set values for started date and retrieve scene and dialogue
+# TODO: im just thinking but we could probably remove dating_active and favour 'visibility' as
+	# our conditional var. this will sacrifice a bit of readability though lmfao idk
 func _date_start():
 	visible = true
 	dating_active = true
@@ -28,37 +45,48 @@ func _date_start():
 	dating_dialogue = DialogueProcessor._get_next_dating_dialogue(current_dating_scene)
 	_display()
 
+# display scene (fill out with info from current dating_dialogue)
 func _display():
+	# set text and images
 	dialogue.text = dating_dialogue["dialogue"]
-	gigiImage.texture = load(dating_dialogue["icon"])
+	gigi_image.texture = load(dating_dialogue["icon"])
 	
+	# generate buttons
 	var options = dating_dialogue["options"]
 	if options.size() > 0:
 		requires_option_selection = true
 		for option in options:
 			var button = Button.new()
+			# set button values
 			button.text = option["option"]
 			button.custom_minimum_size = Vector2(300.0, 100.0)
+			# connect to handler
 			button.pressed.connect(_option_selected.bind(option))
-			buttonContainer.add_child(button)
+			button_container.add_child(button)
 
-func _close_date():
+# ends the current date (returns player to active world)
+func _end_date():
 	dating_active = false
 	# DATE ID WOULD BE INCREMENTED - but for testing sake it has not been...
 	# TODO: create additional date dialogues then increment here
 	date_id = 0
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	visible = false
 
+# option click handler event
 func _option_selected(value: Dictionary):
-	for i in buttonContainer.get_children():
-		i.queue_free()
+	# delete buttons
+	for button in button_container.get_children():
+		button.queue_free()
+	# get dialogue from option selected
 	_increment_date_stage(value)
 	
+# go to next dating dialogue line
 func _increment_date_stage(value: Dictionary):
 	requires_option_selection = false
 	if value["next_id"] == "":
-		_close_date()
+		_end_date()
 	else:
+		# continue dating loop
 		dating_dialogue = DialogueProcessor._get_next_dating_dialogue(current_dating_scene, value)
 		_display()
