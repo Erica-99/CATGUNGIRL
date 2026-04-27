38a67dedecd7ebde622408629e8d0e5d60d1aae4
Emily Hocking <104554365@student.swin.edu.au>
2026-04-27 22:25:51 +1000

    Code review of dialogue links
    Dialogue links within GiGiDate has been switched to use the pre-exisiting DialogueProcessor static branch instead of recreating functions.
    
    Certain functions have been merged into one because I think that looks nicer.
    
    JSON files renamed so they can be dynamically loaded (I love dynamic-ness)

--- scenes/ui/dating/gigi_date.gd
+++ scenes/ui/dating/gigi_date.gd
@@ -1,107 +1,64 @@
 extends Control
 
-var isDating: bool
-var date: Array
-var line
-var file
-var dialogueID = "000001"
-## This is for when there are 3 options instead of 2
-var extraButton: Button
-
-var permit_next := true
+var dating_active: bool
+var date_id: int = 0
+var current_dating_scene = []
+var dating_dialogue = {}
+var requires_option_selection: bool = false
 
 @onready var buttonContainer = $ButtonContainer
 @onready var gigiImage = $gigi_pose
 @onready var dialogue = $date_txt_bgd/date_txt
 
-@export var filePath = "res://resources/dialogue/dates/date_1.json"
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_load_dialogue()
 	visible = false
-	buttonContainer.visible = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("date_show"):
-		if not isDating:
-			_date_start()
-	if isDating && permit_next && event.is_action_pressed("next"):
-		dialogueID = line["next_id"]
-		if _check_dialogue_end():
-			_close_date()
+		if dating_active:
+			if not requires_option_selection:
+				_increment_date_stage(dating_dialogue)
 		else:
-			_set_ui()
+			_date_start()
 
 func _date_start():
 	visible = true
-	isDating = true
-	_set_ui()
+	dating_active = true
+	current_dating_scene = DialogueProcessor._get_dating_scene("date_" + str(date_id), "date_" + str(date_id))
+	dating_dialogue = DialogueProcessor._get_next_dating_dialogue(current_dating_scene)
+	_display()
 
-func _load_dialogue():
-	file = FileAccess.get_file_as_string(filePath)
-	var json = JSON.new()
-	json.parse(file)
-	date = json.data["date_1"]
-
-func _get_dialogue_by_id(id):
-	for dialogue_line in date:
-		if dialogue_line["id"] == id:
-			return dialogue_line
-	return null
-
-func _set_ui():
-	line = _get_dialogue_by_id(dialogueID)
-	_check_has_options()
-	_load_image()
-	_set_dialogue()
-
-func _check_dialogue_end() -> bool:
-	if dialogueID == "":
-		return true
-	else:
-		return false
+func _display():
+	dialogue.text = dating_dialogue["dialogue"]
+	gigiImage.texture = load(dating_dialogue["icon"])
+	
+	var options = dating_dialogue["options"]
+	if options.size() > 0:
+		requires_option_selection = true
+		for option in options:
+			var button = Button.new()
+			button.text = option["option"]
+			button.custom_minimum_size = Vector2(300.0, 100.0)
+			button.pressed.connect(_option_selected.bind(option))
+			buttonContainer.add_child(button)
 
 func _close_date():
-	isDating = false
-	dialogueID = "000001"
-	permit_next = false
+	dating_active = false
+	# DATE ID WOULD BE INCREMENTED - but for testing sake it has not been...
+	# TODO: create additional date dialogues then increment here
+	date_id = 0
 	await get_tree().create_timer(1.0).timeout
 	visible = false
 
-func _check_has_options():
-	if line["has_options"] == true:
-		permit_next = false
-		buttonContainer.visible = true
-		var buttons = buttonContainer.get_children()
-		for i in range(line["options"].size()):
-			var button = Button.new()
-			button.text = line["options"][i]["option"]
-			button.custom_minimum_size = Vector2(300.0, 100.0)
-			button.pressed.connect(_option_selected.bind(i))
-			buttonContainer.add_child(button)
-	else:
-		permit_next = true
-
-func _load_image():
-	var path = line["icon"]
-	var image = load(path)
-	gigiImage.texture = image
-
-func _set_dialogue():
-	dialogue.text = line["dialogue"]
-
-func _option_selected(index):
-	buttonContainer.visible = false
-	dialogueID = line["options"][index]["next_id"]
+func _option_selected(value: Dictionary):
 	for i in buttonContainer.get_children():
-			i.queue_free()
-	if _check_dialogue_end():
+		i.queue_free()
+	_increment_date_stage(value)
+	
+func _increment_date_stage(value: Dictionary):
+	requires_option_selection = false
+	if value["next_id"] == "":
 		_close_date()
 	else:
-		_set_ui()
+		dating_dialogue = DialogueProcessor._get_next_dating_dialogue(current_dating_scene, value)
+		_display()
--- utils/custom_resource_loader.gd
+++ utils/custom_resource_loader.gd
@@ -2,3 +2,4 @@ extends Node
 
 var player_stats = preload("res://resources/player_stats/player_stats.tres")
 var dialogue_path = "res://resources/dialogue/"
+var dating_dialogue_path = "res://resources/dialogue/dates/"
