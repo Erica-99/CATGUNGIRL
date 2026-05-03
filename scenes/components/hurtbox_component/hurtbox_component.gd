7d2ad7b559ed4cb8f5b2ab380bf1013619168147
facelessname <coreychan21@gmail.com>
2026-05-03 12:16:12 +1000

    Fixed enemy hitbox collision layers

--- scenes/components/hurtbox_component/hurtbox_component.gd
+++ scenes/components/hurtbox_component/hurtbox_component.gd
@@ -18,6 +18,7 @@ func _on_area_entered(hitbox: Area3D) -> void:
 
 ## Handles taking damage by making callbacks to health component. Alerts the hitbox that a collision has occured.
 func take_hit(hitbox: Area3D) -> void:
+	print("Hit Taken")
 	hitbox.call("register_hit", self)
 	if hitbox.damage_or_heal_instance != null and health_component != null:
 		health_component.take_damage_or_heal(hitbox.damage_or_heal_instance)
