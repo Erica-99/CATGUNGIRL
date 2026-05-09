420d21231471efe3a246c3b5a872bec0912e5cd2
Emily Hocking <104554365@student.swin.edu.au>
2026-05-09 22:08:54 +1000

    Signals re-linked

--- utils/event_manager.gd
+++ utils/event_manager.gd
@@ -11,3 +11,8 @@ signal enemies_active(is_active: bool)
 signal enemy_killed()
 signal player_hit_by_enemy()
 signal current_killstreak(killstreak: int)
+
+signal player_health_initialised(init_current_health, init_max_health)
+signal player_health_changed(old_health, new_health, damage_or_heal_instance)
+signal player_insanity_gained(amount, buffer)
+signal player_interest_rank_changed(new_rank)
