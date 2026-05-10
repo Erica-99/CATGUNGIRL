02de0a2bd8140eff4041494dc3057a4a5f8579fb
facelessname <coreychan21@gmail.com>
2026-05-10 20:16:53 +1000

    Made progress on music player

--- utils/audio_manager.gd
+++ utils/audio_manager.gd
@@ -1,9 +1,34 @@
 extends Node
 
+@export var music_player: AudioStreamPlayer
+@export var music_tracks: Array[MusicTrack]
+@export var sound_effects: Array[SoundEffect]
 
-# Called when the node enters the scene tree for the first time.
+var sfx_dict: Dictionary
+var music_dict: Dictionary
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-func play_music():
+	# Assign sfx dict keys
+	
+	# Assign music dict keys
+	
 	pass
+
+
+
+
+func play_music(track_ref: String):
+	# Check if valid time to change track
+	# if track is already playing
+	# return
+	# Check if track_ref is valid
+	if music_dict.has(track_ref):
+		# Change Audio Stram to music_track mp3
+		var track: MusicTrack = music_dict[track_ref]
+		music_player.stream = track.music_trac
+		music_player.volume_db = track.volume
+		# Play Stream
+		music_player.play()
+	else:
+		push_error("Audio file not found")
+	
