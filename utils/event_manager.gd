6687fdedafe742602061d8c843898b5705f54c17
Emily Hocking <104554365@student.swin.edu.au>
2026-05-10 19:05:55 +1000

    Enemy manager added and functional
    Also moved GiGiDate and GiGiPopup out of GUI given they require mouse inputs which is frustrating. I'll be doing research on a better way to implement them, but the current setup is at least functional.

--- utils/event_manager.gd
+++ utils/event_manager.gd
@@ -6,8 +6,6 @@ signal activate_date(date_id: int)
 signal begin_date_scene_lock
 signal end_date_scene_lock
 
-signal enemies_active(is_active: bool)
-
 signal increase_insanity_rank
 
 signal enemy_killed()
@@ -18,3 +16,5 @@ signal player_health_initialised(init_current_health, init_max_health)
 signal player_health_changed(old_health, new_health, damage_or_heal_instance)
 signal player_insanity_gained(amount, buffer)
 signal player_interest_rank_changed(new_rank)
+
+signal room_cleared(room_ID: Enums.Room)
