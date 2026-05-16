6e23d60b6b3734516e6128dd1d83c897f2ffad56
facelessname <coreychan21@gmail.com>
2026-05-16 18:19:40 +1000

    Vignett Strength increases at designated insanity intervals. Heartbeat sound plays at Insanity Level 2
    Stilly rough system bandaged together. Needs Polishing

---
+++ scenes/ui/hud_effects.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+@export var vignette: VignetteController
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_update_hud()
+	EventManager.end_date_scene_lock.connect(_update_hud)
+	EventManager.insanity_changed.connect(_update_hud)
+
+func _update_hud() -> void:
+	match Globals.global_insanity_level:
+		0:
+			pass
+		1:
+			vignette.apply_preset(vignette.insanity_low)
+		2:
+			vignette.apply_preset(vignette.insanity_low)
+			AudioManager.play_sfx("heartbeat")
+		3:
+			vignette.apply_preset(vignette.insanity_low)
+		4:
+			vignette.apply_preset(vignette.insanity_low)
+		5:
+			vignette.apply_preset(vignette.insanity_high)
+		_:
+			print("invalid insanity level")
--- scenes/ui/vignette_controller.gd
+++ scenes/ui/vignette_controller.gd
@@ -1,12 +1,11 @@
 extends ColorRect
+class_name VignetteController
 
 @export var insanity_low: VignettePreset
 @export var insanity_high: VignettePreset
 
-func _ready() -> void:
-	_apply_preset(insanity_high)
 
-func _apply_preset(p: VignettePreset) -> void:
+func apply_preset(p: VignettePreset) -> void:
 	material.set("shader_parameter/radius", p.radius)
 	material.set("shader_parameter/softness", p.softness)
 	material.set("shader_parameter/intensity", p.intensity)
