a4886fa9a7847a0022e5c53ed182f7b63a58c0fa
Colby Roberts <103100343@student.swin.edu.au>
2026-05-23 17:13:11 +1000

    Brain jar health bar implemented and shields are working

--- scenes/actors/enemies/brain_jar/brain_jar.gd
+++ scenes/actors/enemies/brain_jar/brain_jar.gd
@@ -1,17 +1,42 @@
 extends Node
 
 var hurtbox
+var shields
+var health
+
+@export var health_bar : ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hurtbox = $HurtboxComponent
+	shields = $ShieldCollision
+	shields.body_entered.connect(_on_body_entered)
+	health_bar.init_health(health)
+	disable_shields()
+
+func _init() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func disable_shields():
-	pass
+func _on_body_entered(body):
+	if body.collision_layer and (1 << 5) != 0:
+		body.queue_free()
 
-func is_hit():
-	if hurtbox.
+func _on_health_component_health_initialised(init_current_health: float, init_max_health: float) -> void:
+	health = init_max_health
+	print(health)
+
+func _on_health_component_health_changed(old_health: float, new_health: float, damage_or_heal_instance: DamageHealInstance) -> void:
+	health = new_health
+	print(health)
+	health_bar.health = health
+	enable_shields()
+
+func disable_shields():
+	shields.monitoring = false
+
+func enable_shields():
+	shields.monitoring = true
