ee191456b53ebebc160d7921417208722661e9ae
Emily Hocking <104554365@student.swin.edu.au>
2026-05-23 22:08:37 +1000

    Fixes for shootablecomponent
    - continued changes for interactablecomponent

--- scenes/components/interactable_component/interactable_component.gd
+++ scenes/components/interactable_component/interactable_component.gd
@@ -2,64 +2,47 @@ extends Node3D
 @onready var interaction_range: Area3D = $"Interaction Range"
 
 @export var interactable_type: Enums.InteractableType = Enums.InteractableType.DOOR
+@export var interactable_load_type: Enums.InteractableLoadType = Enums.InteractableLoadType.BACKGROUND
 @export var interaction_distance: float = 3.0
 @export var event_trigger: Node
 
-var parent_reference
-var player_reference
 var player_in_range: bool = false
 var mesh_size: Vector3
 var tween: Tween
 var initial_position: Vector3
 
+var player_reference
+
 # temp var
 @export var require_interaction: bool = true
 
 func _ready() -> void:
-	parent_reference = get_parent()
-	initial_position = parent_reference.position
-	var parent_mesh_children = parent_reference.find_children("*", "MeshInstance3D", false)
-	if parent_mesh_children.size() > 0:
-		_calculate_interaction_zone(parent_mesh_children[0], false)
-		
-	if interactable_type == Enums.InteractableType.ELEVATOR:
-		_calculate_interaction_zone(0, false)
+	if interactable_load_type != Enums.InteractableLoadType.BACKGROUND_MESHINSTANCE:
+		_calculate_interaction_zone(true, get_parent())
+	else:
+		_calculate_interaction_zone(false)
 
-func _calculate_interaction_zone(child, is_centred):
+func _calculate_interaction_zone(is_using_obj_asset: bool = true, parent: MeshInstance3D = null):
 	#https://forum.godotengine.org/t/is-there-a-way-to-get-the-size-of-a-3d-mesh/23154/3
 	var mesh_box
-	if not interactable_type == Enums.InteractableType.ELEVATOR:
-		mesh_box = child.get_aabb().size
+	if parent != null:
+		mesh_box = parent.get_aabb().size
 	
 	var collision = CollisionShape3D.new()
 	collision.shape = BoxShape3D.new()
 	interaction_range.add_child(collision)
-	#collision.shape.size = mesh_box
 	
-	# all of this shit should be redone
-	# could be ez strategy pattern - just need to set up some prefabs and auto assign
-	# TODO: get to work on this fuckign hell
-	if interactable_type == Enums.InteractableType.CONSOLE:
-		# old shit remove later
-		#var start_interaction_range: Vector3 = Vector3(child.global_position.x, child.global_position.y, child.global_position.z)
-		#var distance_between: float = start_interaction_range.distance_to(child.global_position)
-		#mesh_box.x += distance_between
-		#mesh_box.y += offset_y_amount
-		#mesh_box.z += offset_z_amount
-		#var offsets = Vector3(offset_x_amount, offset_y_amount, distance_between)
-		#collision.shape.size += offsets
-		
+	if interactable_load_type == Enums.InteractableLoadType.BACKGROUND:
 		collision.shape.size = Vector3(mesh_box.z, mesh_box.y, interaction_distance)
 		collision.position = Vector3(0, 0, interaction_distance / 2)
 		
-	elif interactable_type == Enums.InteractableType.ELEVATOR:
-		collision.shape.size = Vector3(5, 5, interaction_distance)
+	elif interactable_load_type == Enums.InteractableLoadType.BACKGROUND_MESHINSTANCE:
+		collision.shape.size = Vector3(interaction_distance, interaction_distance, interaction_distance)
 		collision.position = Vector3(0, 0, interaction_distance / 2)
-	elif interactable_type == Enums.InteractableType.DOOR:
+		
+	elif interactable_load_type == Enums.InteractableLoadType.DOOR:
 		collision.shape.size = Vector3(mesh_box.x + (interaction_distance / 3), mesh_box.y + (interaction_distance / 3), interaction_distance)
-		#collision.position = Vector3(0, 0, interaction_distance / 2)
-		#var offsets = Vector3(1, 1, 1)
-		#collision.shape.size = offsets
+
 
 func _process(delta: float) -> void:
 	if player_in_range:
@@ -87,13 +70,6 @@ func _play_interact_animation(animation_name: String) -> void:
 		tween.tween_property(get_parent(), "position", initial_position, 0.8)
 		pass
 	
-	# old code
-	#var animation_player = parent_reference.find_child("AnimationPlayer", false)
-	#if animation_player != null:
-	#	if animation_player.has_animation(animation_name):
-	#		animation_player.play(animation_name)
-		
-
 func _on_interaction_range_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		player_in_range = true
