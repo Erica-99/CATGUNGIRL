122c36a830b9f398883bd924384dec6d091e6c28
Colby Roberts <103100343@student.swin.edu.au>
2026-05-24 21:27:56 +1000

    Changed math

--- scenes/actors/enemies/brain_jar/brain_jar.gd
+++ scenes/actors/enemies/brain_jar/brain_jar.gd
@@ -18,7 +18,7 @@ func _on_health_component_health_initialised(init_current_health: float, init_ma
 	print(health)
 
 func _on_health_component_health_changed(old_health: float, new_health: float, damage_or_heal_instance: DamageHealInstance) -> void:
-	if (old_health - new_health) != (health_bar.max_value / 5):
+	if ((old_health - new_health) > (health_bar.max_value / 5)) or ((old_health - new_health) < (health_bar.max_value / 5)):
 		new_health = health - (health_bar.max_value / 5)
 	health = new_health
 	print(health)
