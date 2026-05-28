396cd69ec8531f3ac9c62018db38697e6c434426
facelessname <coreychan21@gmail.com>
2026-05-28 16:16:49 +1000

    Multiple levels added for screen pulse. Date popup renders over HUD again

--- scenes/ui/insanity_screenpulse.gd
+++ scenes/ui/insanity_screenpulse.gd
@@ -1,3 +1,12 @@
 extends InsanityEffect
 
 @export var screen_pulse: ScreenPulseController
+
+@export var insanity_low: ScreenPulsePreset
+@export var insanity_high: ScreenPulsePreset
+
+func change_effect_2() -> void:
+	screen_pulse.apply_preset(insanity_low)
+
+func change_effect_4() -> void:
+	screen_pulse.apply_preset(insanity_high)
