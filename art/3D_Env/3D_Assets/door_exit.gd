27857f22d8c8204f4942b55d48b2030a8903ba6c
Colby Roberts <103100343@student.swin.edu.au>
2026-05-28 16:46:24 +1000

    Door Light Colour changes

--- art/3D_Env/3D_Assets/door_exit.gd
+++ art/3D_Env/3D_Assets/door_exit.gd
@@ -1,21 +1,24 @@
 extends StaticBody3D
 
-var door_light_1
-var door_light_2
-var is_cleared
+@export var level_end: Area3D
+
 var unlocked_colour
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	door_light_1 = $EXITHERECYLINDER
-	door_light_2 = $EXITHERECYLINDER2
-	
-	is_cleared = true
-	
+	##Create the unlocked colour for door lights
 	unlocked_colour = StandardMaterial3D.new()
 	unlocked_colour.albedo_color = Color(0 ,1.0, 0)
 	unlocked_colour.emission_color = Color(0, 1.0, 0)
+	
+	##Don't allow the player to progress until all enemies are cleared
+	#level_end.monitoring = false
 
 func _change_door_lighting():
-	if is_cleared == true:
-		door_light_1.material_override = unlocked_colour
+	$EXITHERECYLINDER.material_override = unlocked_colour
+	$EXITHERECYLINDER2.material_override = unlocked_colour
+
+
+func _on_enemy_manager_stage_cleared() -> void:
+	_change_door_lighting()
+	level_end.monitoring = true
