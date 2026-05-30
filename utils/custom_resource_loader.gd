658f88e32affef5d0151c6cd923adb90893681eb
Emily Hocking <104554365@student.swin.edu.au>
2026-05-30 16:01:09 +1000

    Fixes to enemy spawner
    + enemy spawner can now handle multiple types of enemies (currently just convict and scrub, but its super simple to add more)

--- utils/custom_resource_loader.gd
+++ utils/custom_resource_loader.gd
@@ -4,3 +4,4 @@ var player_stats = preload("res://resources/player_stats/player_stats.tres")
 var dialogue_path = "res://resources/dialogue/"
 var dating_dialogue_path = "res://resources/dialogue/dates/"
 var popup_dialogue_path = "res://resources/dialogue/popups/"
+var enemy_path = "res://scenes/actors/enemies/"
