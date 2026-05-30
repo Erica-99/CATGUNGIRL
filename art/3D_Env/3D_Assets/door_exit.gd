6ab99b9960af71ae937644a0920f0ab79e3763fd
Colby Roberts <103100343@student.swin.edu.au>
2026-05-30 22:24:36 +1000

    Troubleshooting door disable on level transition

--- art/3D_Env/3D_Assets/door_exit.gd
+++ art/3D_Env/3D_Assets/door_exit.gd
@@ -1,18 +1,20 @@
 extends StaticBody3D
 
-@export var level_end: Area3D
-
 var unlocked_colour
+
+var level_end
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().process_frame
+	level_end = find_child("LevelEnd")
 	##Create the unlocked colour for door lights
 	unlocked_colour = StandardMaterial3D.new()
 	unlocked_colour.albedo_color = Color(0 ,1.0, 0)
 	unlocked_colour.emission_color = Color(0, 1.0, 0)
 	
 	##Don't allow the player to progress until all enemies are cleared
-	#level_end.monitoring = false
+	level_end.monitoring = false
 
 func _change_door_lighting():
 	$EXITHERECYLINDER.material_override = unlocked_colour
