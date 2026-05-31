192602c31d88b64c9b55939a8869aeb009116da6
Colby Roberts <103100343@student.swin.edu.au>
2026-05-31 13:56:39 +1000

    Added comments to code + Fixed door disabling
    Took me embarrassingly long to find the simple fix for it...

--- art/3D_Env/3D_Assets/door_exit.gd
+++ art/3D_Env/3D_Assets/door_exit.gd
@@ -1,20 +1,18 @@
 extends StaticBody3D
 
 var unlocked_colour
-
 var level_end
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	##Get the LevelEnd component
 	await get_tree().process_frame
-	level_end = find_child("LevelEnd")
+	level_end = find_child("LevelEnd", true, false)
 	##Create the unlocked colour for door lights
 	unlocked_colour = StandardMaterial3D.new()
 	unlocked_colour.albedo_color = Color(0 ,1.0, 0)
 	unlocked_colour.emission_color = Color(0, 1.0, 0)
-	
-	##Don't allow the player to progress until all enemies are cleared
-	level_end.monitoring = false
+
 
 func _change_door_lighting():
 	$EXITHERECYLINDER.material_override = unlocked_colour
