6823d12a4c9de258222ad42d6fdc44465e61b83c
facelessname <coreychan21@gmail.com>
2026-06-11 20:49:37 +1000

    Bubble popup disappears after date and pre date sequence locks input

--- scenes/actors/player/gigi_bubble.gd
+++ scenes/actors/player/gigi_bubble.gd
@@ -1,13 +1,14 @@
 extends AnimatedSprite3D
 
-
-
 func _ready() -> void:
 	visible = false
 	EventManager.pre_date_sequence.connect(_pre_date_sequence)
 
 func _pre_date_sequence(date_id: int) -> void:
 	visible = true
+	EventManager.begin_date_scene_lock.emit()
 	play()
 	await get_tree().create_timer(2).timeout
 	EventManager.activate_date.emit(date_id)
+	visible = false
+	stop()
