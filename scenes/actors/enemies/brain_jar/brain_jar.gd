5df2c04559edf67294585348404e2649637f9a62
TStellSwin <105599464@student.swin.edu.au>
2026-06-12 01:06:42 +1000

    Spawns enemies on shield enable
    Integrated Enemy_Enhancements to spawn enemies when shield is enabled.

--- scenes/actors/enemies/brain_jar/brain_jar.gd
+++ scenes/actors/enemies/brain_jar/brain_jar.gd
@@ -25,6 +25,8 @@ func _on_health_component_health_changed(old_health: float, new_health: float, d
 	health_bar.health = health
 	if health > 0:
 		_enable_shields()
+		EventManager.spawn_enemy.emit(0.1, get_path_to($"../EnemyManager/EnemySpawner"))
+		EventManager.spawn_enemy.emit(0.1, get_path_to($"../EnemyManager/EnemySpawner2"))
 	else:
 		# Play out death sequence (e.g. animations, cutscene)
 		queue_free()
