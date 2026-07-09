edcf533eaa8d84aebf9305cbd77dd8bdd7b8999c
Fernando-105343111 <105343111@student.swin.edu.au>
2026-07-09 13:47:13 +1000

    Wall sliding and jumping
    Added wall slide and wall jump movement system. The player can now transition into a wall slide from both the jump and fall states when they make contact with a valid wall, and can then jump away from the wall. I also added shared wall detection in player.gd so the jump and fall states can use the same logic, plus exported wall movement variables so the slide speed, stick velocity, wall jump force, control lock time, and wall angle can be adjusted from the editor. I also added notes for the new variables.
    
    Some issues:
    The player gets its movement cancelled when jumping off a wall, I think this has something to do with the way the player_move.gd checks for the direction the player was moving last frame.
    Some objects don't make sense to have the player slide off from. I was thinking of making a group for objects that can be slided on, but not sure if that's the best way, plus I don't want to mess to much with things without permission.

---
+++ scenes/components/state_machine/states/player/player_wall_jump.gd
@@ -0,0 +1,49 @@
+extends State
+class_name PlayerWallJump
+
+var actor: CharacterBody3D
+var input_component: InputComponent
+
+var wall_jump_dir: float = 0.0
+var control_lock_timer: float = 0.0
+
+func init(blackboard_dict : Dictionary) -> void:
+	super(blackboard_dict)
+	actor = blackboard["actor"]
+	input_component = blackboard["input_component"]
+	
+func enter() -> void:
+	wall_jump_dir = blackboard.get("wall_jump_dir", 0.0)
+	control_lock_timer = actor.wall_jump_control_lock_time
+	
+	actor.set_facing(wall_jump_dir)
+	actor.velocity.x = wall_jump_dir * actor.wall_jump_horizontal_velocity
+	actor.velocity.y = actor.wall_jump_vertical_velocity
+	
+func exit() -> void:
+	pass
+	
+func update(_delta: float) -> void:
+	if actor.is_on_floor():
+		if actor.velocity.x == 0:
+			transitioned.emit(self, "playeridle")
+		else:
+			transitioned.emit(self, "playermove")	
+	elif actor.velocity.y < 0:
+		transitioned.emit(self, "playerfall")
+		
+func physics_update(_delta: float) -> void:
+	if not actor.is_on_floor():
+		actor.velocity += actor.get_gravity() * _delta
+		
+	if control_lock_timer > 0.0:
+		control_lock_timer -= _delta
+	else:
+		var input_state = input_component.get_input_state()
+		var input_dir: float = input_state["movement"]
+		var direction := (actor.transform.basis * Vector3(input_dir, 0, 0)).normalized()
+		
+		actor.velocity.x += direction.x * actor.air_acceleration * _delta
+		actor.velocity.x = clampf(actor.velocity.x, -actor.wall_jump_horizontal_velocity, actor.wall_jump_horizontal_velocity)
+		
+	actor.move_and_slide()
