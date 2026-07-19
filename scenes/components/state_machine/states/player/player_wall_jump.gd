40503004a110dbf5acab28cdf8a840a393a1af43
Erica-99 <turbocake17@gmail.com>
2026-07-19 21:18:18 +1000

    Add grappling to wall states. Fixed disconnected facing signal. Made wall slide override player facing direction

--- scenes/components/state_machine/states/player/player_wall_jump.gd
+++ scenes/components/state_machine/states/player/player_wall_jump.gd
@@ -24,11 +24,14 @@ func exit() -> void:
 	pass
 	
 func update(_delta: float) -> void:
+	var input_state = input_component.get_input_state()
 	if actor.is_on_floor():
 		if actor.velocity.x == 0:
 			transitioned.emit(self, "playeridle")
 		else:
-			transitioned.emit(self, "playermove")	
+			transitioned.emit(self, "playermove")
+	elif input_state["ability_held"] and blackboard["gun_holder"].current_gun.ability:
+		transitioned.emit(self, "playerability")
 	elif actor.velocity.y < 0:
 		transitioned.emit(self, "playerfall")
 		
