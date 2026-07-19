5a3e8925ebaf9a1f9057a02cc8ce9ad057a3faf3
facelessname <coreychan21@gmail.com>
2026-07-19 19:13:57 +1000

    Added button for spawning missilies in test level

--- scenes/missile/missile_launcher.gd
+++ scenes/missile/missile_launcher.gd
@@ -6,3 +6,8 @@ func launch_missile() -> void:
 	var new_missile = MISSILE.instantiate()
 	
 	add_child(new_missile)
+	print("Missile Launched")
+
+
+func _on_button_pressed() -> void:
+	launch_missile()
