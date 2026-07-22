e3410f52792696b01b872989487bea5e415c72a5
Wacch0wski <162528349+Wacch0wski@users.noreply.github.com>
2026-07-22 17:27:10 +1000

    Trunk Visual Assets
    Added all assets and nodes needed to start programming sprite movement

--- scenes/actors/enemies/trunk/trunk_visuals.gd
+++ scenes/actors/enemies/trunk/trunk_visuals.gd
@@ -1,5 +1,6 @@
 extends Node3D
-@onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
+@onready var animated_sprite_3d: AnimatedSprite3D = $Torso/AnimatedSprite3D
+@onready var sprite_pivot = $Torso
 @onready var platform_check: RayCast3D = $"../PlatformCheck"
 @onready var object_check: RayCast3D = $"../ObjectCheck"
 # chuck all visual code stuff and gun/missile aiming here
@@ -7,9 +8,10 @@ extends Node3D
 func _on_trunk_facing_changed(trunk: CharacterBody3D) -> void:
 	if trunk.facing == -1.0:
 		#legs_p.scale.x = -1
-		animated_sprite_3d.scale.x = -0.45
+		
+		sprite_pivot.scale.x = -1
 	else:
-		animated_sprite_3d.scale.x = 0.45
+		sprite_pivot.scale.x = 1
 	
 	platform_check.target_position.x = -platform_check.target_position.x
 	object_check.target_position.x = -object_check.target_position.x
