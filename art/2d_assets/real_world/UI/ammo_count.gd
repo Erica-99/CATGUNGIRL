330ba874e8da821051d65509a77c6aa1206de4e5
Erica-99 <turbocake17@gmail.com>
2026-07-24 14:48:24 +1000

    Bad inital implementation of ammo icon swapping implemented

--- art/2d_assets/real_world/UI/ammo_count.gd
+++ art/2d_assets/real_world/UI/ammo_count.gd
@@ -1,15 +1,19 @@
 extends Control
 
-@onready var ui_bullet_icon = preload("res://art/2d_assets/real_world/UI/bullet_icon.tscn")
+#@onready var ui_bullet_icon = preload("res://art/2d_assets/real_world/UI/bullet_icon.tscn")
+@export var bullet_icons: Dictionary[String, PackedScene]
 
 @onready var VBOX = $ColorRect/MarginContainer/VBoxContainer
 
 signal bulletlost
 
+var current_ammo_icon: PackedScene
+
 func _ready() -> void:
 	EventManager.new_mag_loaded.connect(_load_new_mag)
 	EventManager.shot_fired.connect(_fire_shot)
 	EventManager.shots_loaded.connect(_add_bullets)
+	EventManager.new_gun_equipped.connect(_ammo_type_changed)
 
 
 func _load_new_mag(ammo: int, mag_capacity: int) -> void:
@@ -21,7 +25,7 @@ func _load_new_mag(ammo: int, mag_capacity: int) -> void:
 	
 	# Load new bullets
 	for i in ammo:
-		var newbullet = ui_bullet_icon.instantiate()
+		var newbullet = current_ammo_icon.instantiate()
 		VBOX.add_child(newbullet)
 
 
@@ -33,5 +37,11 @@ func _fire_shot() -> void:
 
 func _add_bullets(bullets_to_add: int) -> void:
 	for bullet in bullets_to_add:
-		var newbullet = ui_bullet_icon.instantiate()
+		var newbullet = current_ammo_icon.instantiate()
 		VBOX.add_child(newbullet)
+
+func _ammo_type_changed(new_type: String) -> void:
+	if new_type in bullet_icons.keys():
+		current_ammo_icon = bullet_icons[new_type]
+	else:
+		bullet_icons.values()[0] # Just default to the first item (should be pistol ammo)
--- scenes/components/gun_poc/gun_holder.gd
+++ scenes/components/gun_poc/gun_holder.gd
@@ -33,8 +33,8 @@ func _ready() -> void:
 	# lazy code - should be changed to consider other child types...
 	current_gun = get_child(current_gun_index)
 	_activate_gun()
-	EventManager.new_mag_loaded.emit(current_gun._current_ammo, current_gun.ammo_max)
 	EventManager.new_gun_equipped.emit(current_gun.gun_name)
+	EventManager.new_mag_loaded.emit(current_gun._current_ammo, current_gun.ammo_max)
 
 func _process(delta: float) -> void:
 	var current_input_state = input_component.get_input_state()
@@ -54,8 +54,8 @@ func _switch_gun():
 	print("GUN SWITCHED TO: ")
 	print(current_gun)
 	
-	EventManager.new_mag_loaded.emit(current_gun._current_ammo, current_gun.ammo_max)
 	EventManager.new_gun_equipped.emit(current_gun.gun_name)
+	EventManager.new_mag_loaded.emit(current_gun._current_ammo, current_gun.ammo_max)
 
 func _deactivate_gun():
 	current_gun.active = false
