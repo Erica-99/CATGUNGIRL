010eba18722ef81c174455695f68ef85ab80ade8
facelessname <coreychan21@gmail.com>
2026-07-25 04:11:34 +1000

    Adjusted Sprite Offset and collisionshape, so the missile turns more naturally. Tweaked Values

--- scenes/components/state_machine/states/missile/missile_spawn.gd
+++ scenes/components/state_machine/states/missile/missile_spawn.gd
@@ -4,8 +4,6 @@ class_name MissileSpawn
 @export var burst_duration := 0.5
 @export var burst_velocity:= 4
 @export var burst_damp = 5
-@export var spin_count: int = 4
-@export var spin_damp := 1
 
 
 var timer := 0.0
@@ -19,20 +17,17 @@ func enter():
 	body.velocity.z = 0
 	body.velocity.y = burst_velocity
 	timer = 0.0
-	spin_progress = spin_count * 360
 
 func physics_update(delta):
 	timer += delta
 	
-	spin_progress = lerp(spin_progress, 0, spin_damp * delta)
-	body.rotation.z = deg_to_rad(spin_progress)
 	
 	if timer <= burst_duration:
 		body.velocity.y = burst_velocity
 		
 	else:
 		body.velocity.y = lerp(body.velocity.y, 0.0, burst_damp * delta)
-		if body.velocity.y < 0.1 and body.rotation.z < 0.1:
+		if body.velocity.y < 0.1:
 			transitioned.emit(self, "missilehome")
 			print("Target Locked")
 	
