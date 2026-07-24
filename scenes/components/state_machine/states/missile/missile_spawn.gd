c62c26364dc11c65359ca9d30a88e7e9aa0168ef
facelessname <coreychan21@gmail.com>
2026-07-25 04:28:51 +1000

    Missile spawn with a randomised launch direction (within a range facing upwards

--- scenes/components/state_machine/states/missile/missile_spawn.gd
+++ scenes/components/state_machine/states/missile/missile_spawn.gd
@@ -8,14 +8,19 @@ class_name MissileSpawn
 
 var timer := 0.0
 var body: CharacterBody3D
-var spin_progress: int
+var launch_dir: Vector2
 
 
 func enter():
 	body = blackboard["actor"]
-	body.velocity.x = 0
-	body.velocity.z = 0
-	body.velocity.y = burst_velocity
+	
+	var cone_radius := 0.4
+	var x := randf_range(-cone_radius, cone_radius)
+	var y := 1.0
+
+	launch_dir = Vector2(x, y).normalized()
+	
+	
 	timer = 0.0
 
 func physics_update(delta):
@@ -23,9 +28,11 @@ func physics_update(delta):
 	
 	
 	if timer <= burst_duration:
-		body.velocity.y = burst_velocity
+		body.velocity.x = burst_velocity * launch_dir.x
+		body.velocity.y = burst_velocity * launch_dir.y
 		
 	else:
+		body.velocity.x = lerp(body.velocity.x, 0.0, burst_damp * delta)
 		body.velocity.y = lerp(body.velocity.y, 0.0, burst_damp * delta)
 		if body.velocity.y < 0.1:
 			transitioned.emit(self, "missilehome")
