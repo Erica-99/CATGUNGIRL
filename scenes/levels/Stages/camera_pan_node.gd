68af21b57545821160d0560fb93ecf8baa740148
Wacch0wski <162528349+Wacch0wski@users.noreply.github.com>
2026-08-07 14:07:40 +1000

    Camera Panning node functionality
    Spaghettied together instantiated Camera panning nodes that will allow a limited degree of control on a new camera, while also adding some extra functionality.

--- scenes/levels/Stages/camera_pan_node.gd
+++ scenes/levels/Stages/camera_pan_node.gd
@@ -1,23 +1,35 @@
 extends Node3D
 
-signal updatecameratween
+signal updatecameratween(NewCameraTarget,FOVtarget,RotationTarget)
 signal removecameratween
 
 @onready var cameratweentarget = $CameraTarget
 var targetpos
-@export var FOV = 75
+var targetrot
+@export var FOV = 0
+@export var reset_on_exit = false
+@export var Reset_Camera_on_Entry = false
+@export var look_at_player = false
+var targetposition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	targetpos = cameratweentarget.position
+	targetpos = cameratweentarget.global_position
+	targetrot = cameratweentarget.rotation
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_body_entered(body: Node3D) -> void:
+	if Reset_Camera_on_Entry == false:
+		print('area Entered')
+		updatecameratween.emit(targetpos,FOV,targetrot,look_at_player)
+	else:
+		removecameratween.emit()
+	pass # Replace with function body.
 
 
-func _on_area_area_entered(area: Area3D) -> void:
-	
+func _on_area_body_exited(body: Node3D) -> void:
+	if reset_on_exit == true:
+		removecameratween.emit()
 	pass # Replace with function body.
--- scenes/levels/Stages/panning_camera.gd
+++ scenes/levels/Stages/panning_camera.gd
@@ -3,13 +3,23 @@ extends Node3D
 
 
 
-
+@onready var camera = $Camera
 @onready var player = $"../Player"
-
+@onready var playerrotate = $Player_rotation_pivot
 @export var default_offset = Vector3(0,20,45)
+@export var CameraSpeed = 6
 
-var playercam_pos
+##The level of influence the target point has on the camera, 0 focuses on player, 1 focuses on target
+@export var Player_Target_Ratio = 0.5
+
+
+var looking_at_player = false
 var targetpos
+var targetfov
+var playercam_pos
+var enviroment_offset_active = false
+var enviroment_offset_Value
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,12 +28,42 @@ func _ready() -> void:
 
 
 func gettargetposition():
-	playercam_pos = Vector3(player.position.x,player.position.y,player.position.z)
-	targetpos = playercam_pos + default_offset
-	return targetpos
+	if enviroment_offset_active == false:
+		playercam_pos = Vector3(player.position.x,player.position.y,player.position.z)
+		targetpos = playercam_pos + default_offset
+		return targetpos
+	else:
+		playercam_pos = Vector3(player.position.x,player.position.y,player.position.z)
+		var playercamtarget = playercam_pos + default_offset
+		targetpos = playercamtarget.lerp(enviroment_offset_Value,Player_Target_Ratio)
+		return targetpos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	targetpos = gettargetposition()
-	position = targetpos
+	playerrotate.look_at(player.position)
+	
+	if looking_at_player == true:
+		rotation = rotation.lerp(playerrotate.rotation,CameraSpeed * delta)
+	else:
+		rotation = rotation.lerp(Vector3(0,0,0), CameraSpeed * delta)
+	targetpos = gettargetposition() 
+	position = position.lerp(targetpos,CameraSpeed * delta)
 	pass
+
+
+func _on_camera_pan_node_updatecameratween(NewCameraTarget: Variant, FOVtarget: Variant, RotationTarget: Variant,is_looking_at_player) -> void:
+	print(NewCameraTarget)
+	looking_at_player = is_looking_at_player
+	enviroment_offset_active = true
+	enviroment_offset_Value = NewCameraTarget
+	targetfov = 75 + FOVtarget
+	var fov_tween = create_tween()
+	fov_tween.tween_property(camera,'fov', targetfov,0.5)
+	pass # Replace with function body.
+
+
+func _on_camera_pan_node_removecameratween() -> void:
+	enviroment_offset_active = false
+	targetfov = 75
+	looking_at_player = false
+	pass # Replace with function body.
