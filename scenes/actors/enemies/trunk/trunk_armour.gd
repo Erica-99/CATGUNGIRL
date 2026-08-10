29220d8c8dd3aedd303ed6e4b58a99be7c265329
Emily Hocking <104554365@student.swin.edu.au>
2026-08-10 21:54:27 +1000

    setting up trunk armour
    adding armour and initial code

---
+++ scenes/actors/enemies/trunk/trunk_armour.gd
@@ -0,0 +1,40 @@
+extends Area3D
+
+@export var team_component: Node
+@export var hit_sfx_ref: String
+@export var minimum_damage_to_break: int
+@export var is_broken: bool = false
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(hitbox: Area3D) -> void:
+	if team_component == null or hitbox.team_component == null:
+		take_hit(hitbox)
+	elif team_component.team != hitbox.team_component.team:
+		take_hit(hitbox)
+	else:
+		return
+
+## Handles taking damage by making callbacks to health component. Alerts the hitbox that a collision has occured.
+# altered from original hurtbox component - just for armour
+func take_hit(hitbox: Area3D) -> void:
+	if hit_sfx_ref != "":
+		AudioManager.play_sfx(hit_sfx_ref)
+	hitbox.call("register_hit", self)
+	if hitbox.damage_or_heal_instance != null:
+		print(hitbox.damage_or_heal_instance)
+		print(hitbox)
+		
+		if hitbox.damage_or_heal_instance.amount >= 30:
+			print("I WAS HIT WITH A SNIPER")
+		
+		#var dmg_heal_instance = hitbox.damage_or_heal_instance.duplicate()
+		## apply damage multiplier to damage. ignore multipliers above 1.0 if damage is explosive since it doesnt make sense for explosive damage to headshot.
+		#if dmg_heal_instance.type == Enums.DamageType.EXPLOSIVE:
+			#dmg_heal_instance.amount *= clampf(damage_multiplier, 0, 1)
+		#else:
+			#dmg_heal_instance.amount *= damage_multiplier
+		#health_component.take_damage_or_heal(dmg_heal_instance)
+		#hitbox.call("register_damage_dealt", dmg_heal_instance.amount * float(not dmg_heal_instance.is_heal))
+	
