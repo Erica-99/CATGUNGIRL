3f6e17e17f5a4fe3af24f34b2e397468257f8ef0
Fernando-105343111 <105343111@student.swin.edu.au>
2026-08-22 10:47:51 +1000

    convict chase fix
    fixed issue where convicts would not be able to reach player if blocked by something and in the same y value. Also fixed issue where convicts would get stuck in a loop of walking back to a route point and power diving when above the player and blocked by something above.

--- scenes/components/state_machine/states/enemy/convict/convict_chase.gd
+++ scenes/components/state_machine/states/enemy/convict/convict_chase.gd
@@ -31,6 +31,7 @@ var dive_launch_force: float
 var convict_route_points: Array = []
 var current_route_point: Node3D = null
 var route_point_reached_distance: float = 1.0
+var routing_to_dive_spot: bool = false
 
 func init(blackboard_dict: Dictionary) -> void:
 	super(blackboard_dict)
@@ -57,6 +58,7 @@ func enter() -> void:
 	
 	dive_timer = 0
 	current_route_point = null
+	routing_to_dive_spot = false
 
 func _start_cooldown() -> void:
 	await get_tree().create_timer(randf_range(attack_cooldown_min, attack_cooldown_max)).timeout
@@ -87,6 +89,10 @@ func _pick_route_point() -> Node3D:
 			closest_point = point
 	
 	return closest_point
+	
+func _has_power_dive_space() -> bool:
+	var estimated_dive_height: float = (dive_launch_force * dive_launch_force) / (2.0 * gravity)
+	return !actor.test_move(actor.global_transform, Vector3(0.0, estimated_dive_height, 0.0))
 
 func physics_update(_delta: float) -> void:
 	if actor.global_position.x > target.global_position.x:
@@ -94,8 +100,14 @@ func physics_update(_delta: float) -> void:
 	elif actor.global_position.x < target.global_position.x:
 		direction = 1
 	
-	# While beneath target, add to super jump cooldown
-	if actor.global_position.y < target.global_position.y:
+	var player_is_above: bool = actor.global_position.y < target.global_position.y
+	var player_is_below: bool = actor.global_position.y > target.global_position.y + 1.0
+	
+	if player_is_below:
+		routing_to_dive_spot = false
+		current_route_point = null
+	# add to dive timer while below target
+	if player_is_above:
 		dive_timer += _delta
 	else:
 		dive_timer = 0
@@ -103,14 +115,13 @@ func physics_update(_delta: float) -> void:
 	var searching_for_dive_spot: bool = false
 	# When below target for long enough, move to superjump state
 	if dive_timer > dive_cd:
-		var estimated_dive_height := (dive_launch_force * dive_launch_force) / (2.0 * gravity)
-		var has_space_above := !actor.test_move(actor.global_transform, Vector3(0.0, estimated_dive_height, 0.0))
-		
-		if has_space_above:
+		if _has_power_dive_space():
 			current_route_point = null
+			routing_to_dive_spot = false
 			transitioned.emit(self, "convictpowerdive")
 			return
 		else:
+			routing_to_dive_spot = true
 			searching_for_dive_spot = true
 	
 	# offset target position
@@ -118,14 +129,25 @@ func physics_update(_delta: float) -> void:
 	var move_direction: float = 0.0
 	var trying_to_run: bool = false
 	var blocked_ahead: bool = false
+	var using_route_point: bool = false
 	
 	# height/distance checks for superjump (UNCOMMENT TO TEST SUPERJUMP)
 	#var height_difference = target.global_position.y - actor.global_position.y
 	#var horizontal_difference = abs(actor.global_position.x - target.global_position.x)
 	
 	# check if convict is already in an acceptable position
-	if searching_for_dive_spot and !convict_route_points.is_empty():
-		if current_route_point == null or abs(actor.global_position.x - current_route_point.global_position.x) <= route_point_reached_distance:
+	if (searching_for_dive_spot or routing_to_dive_spot) and !convict_route_points.is_empty():
+		using_route_point = true
+
+		if _has_power_dive_space():
+			current_route_point = null
+			routing_to_dive_spot = false
+			transitioned.emit(self, "convictpowerdive")
+			return
+		
+		var reached_route_point: bool = current_route_point != null and abs(actor.global_position.x - current_route_point.global_position.x) <= route_point_reached_distance
+		
+		if current_route_point == null or reached_route_point:
 			current_route_point = _pick_route_point()
 		
 		if current_route_point != null:
@@ -150,6 +172,10 @@ func physics_update(_delta: float) -> void:
 	if trying_to_run:
 		blocked_ahead = actor.test_move(actor.global_transform, Vector3(move_direction * 0.4, 0.0, 0.0))
 		
+		if blocked_ahead and !using_route_point:
+			routing_to_dive_spot = true
+			current_route_point = null
+	
 	actor.move_and_slide()
 	
 	if !trying_to_run or blocked_ahead:
