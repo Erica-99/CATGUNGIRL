0d3e03e34a25462c84b8b60f41fd9c7ebf8709be
Michael Adamopoulos <105338010@student.swin.edu.au>
2026-08-25 03:02:48 +1000

    WIP of getting enemies spawning after anim. Part 2

--- art/3D_Env/3D_Assets/3D_Assets_Scenes/Env_Scenes/0_Multi/prison_cell_active_test_ver.gd
+++ art/3D_Env/3D_Assets/3D_Assets_Scenes/Env_Scenes/0_Multi/prison_cell_active_test_ver.gd
@@ -2,6 +2,7 @@ extends StaticBody3D
 
 #@onready var Cell_Anims = $AnimationPlayer
 @export var cell_anim_player: AnimationPlayer
+@export var enemy_spawner: EnemySpawner
 
 func _ready() -> void:
 	EventManager.connect("start_animation", _on_start_animation)
@@ -15,4 +16,5 @@ func _on_start_animation() -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Cell_Opening":
+		EventManager.emit_signal("spawn_enemy", 2.0, enemy_spawner)
 		print("Enemy should spawn now")
