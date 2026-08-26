72530d769e7f18ead1a73f4a1cd4f9f55f106e37
Michael Adamopoulos <105338010@student.swin.edu.au>
2026-08-26 20:29:16 +1000

    WIP of spawning enemies, more working, not 100%

--- art/3D_Env/3D_Assets/3D_Assets_Scenes/Env_Scenes/0_Multi/prison_cell_active_test_ver.gd
+++ art/3D_Env/3D_Assets/3D_Assets_Scenes/Env_Scenes/0_Multi/prison_cell_active_test_ver.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 	EventManager.connect("start_animation", _on_start_animation)
 	cell_anim_player.animation_finished.connect(_on_animation_player_animation_finished)
 	
-	var spawner_path = enemy_spawner.get_path()
+	spawner_path = enemy_spawner.get_path()
+	print("spawner_path 1 = ", spawner_path)
 	
 	cell_anim_player.play("Cell_IdleClosed")
 
@@ -19,5 +20,8 @@ func _on_start_animation() -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Cell_Opening":
-		EventManager.emit_signal("spawn_enemy", 2.0, spawner_path)
+		EventManager.emit_signal("spawn_enemy", 0.1, spawner_path)
+		print("spawner_path 1.5 = ", spawner_path)
+		#.call
+		#custom_delay: float, spawner_path: NodePath
 		print("Enemy should spawn now")
