dbe2a04f669e4f6d6a26008aef6de936910d83fc
Fernando-105343111 <105343111@student.swin.edu.au>
2026-08-27 23:13:49 +1000

    No aggro
    When debug option is on, enemies will remain in their current state and be unable to switch into another state (unless it is a death state)

--- scenes/components/state_machine/state_machine.gd
+++ scenes/components/state_machine/state_machine.gd
@@ -9,6 +9,9 @@ signal state_changed(prev: String, new: String)
 
 @export var initial_state: State
 
+@export_category("Debug")
+@export var block_state_transitions: bool = true
+
 var current_state : State
 var current_state_name: String
 var states : Dictionary = {}
@@ -27,10 +30,14 @@ func init(blackboard: Dictionary = {}) -> void:
 		current_state = initial_state
 
 func _process(delta: float) -> void:
+	_handle_debug_no_aggro()
+	
 	if current_state:
 		current_state.update(delta)
 
 func _physics_process(delta: float) -> void:
+	_handle_debug_no_aggro()
+	
 	if current_state:
 		current_state.physics_update(delta)
 
@@ -38,14 +45,49 @@ func on_child_transition(state: State, new_state_name: String):
 	if state != current_state:
 		return
 	
+	if block_state_transitions and DebugManager.no_aggro and !_debug_allowed_transition(new_state_name):
+		return
+	
 	var new_state = states.get(new_state_name.to_lower())
 	if !new_state:
 		return
 	
+	_change_state(new_state)
+
+
+func _handle_debug_no_aggro() -> void:
+	if !block_state_transitions:
+		return
+	
+	if !DebugManager.no_aggro:
+		return
+	
+	if initial_state == null:
+		return
+	
+	if current_state == null:
+		return
+	
+	if current_state == initial_state:
+		return
+	
+	if _debug_allowed_transition(current_state.name):
+		return
+	
+	_change_state(initial_state)
+
+func _debug_allowed_transition(state_name: String) -> bool:
+	return state_name.to_lower().contains("death")
+
+func _change_state(new_state: State) -> void:
+	var previous_state_name: String = ""
+	
 	if current_state:
+		previous_state_name = current_state.name.to_lower()
 		current_state.exit()
 	
 	new_state.enter()
 	current_state = new_state
+	current_state_name = new_state.name.to_lower()
 	
-	state_changed.emit(state.name.to_lower(), new_state_name)
+	state_changed.emit(previous_state_name, current_state_name)
