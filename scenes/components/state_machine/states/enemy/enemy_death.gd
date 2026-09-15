e1375e436fb54b49ff6d11b73181b08cd6a15f6e
Wacch0wski <162528349+Wacch0wski@users.noreply.github.com>
2026-09-15 16:41:27 +1000

    blood improvements and trunk corpses

--- scenes/components/state_machine/states/enemy/enemy_death.gd
+++ scenes/components/state_machine/states/enemy/enemy_death.gd
@@ -3,10 +3,10 @@ extends State
 class_name EnemyDeath
 
 @export var max_death_animation_time: float = 2
-@export var explode_on_kill: bool
+@export var leave_trunk_corpse_on_kill: bool
 var death_gpu_emitter = preload("res://scenes/VFX/blood_spurt_medium_vfx.tscn")
 var explosion_gpu_emitter = preload("res://art/TechArt/1_Shaders/explosion_test.tscn")
-
+var trunk_corpse = preload('res://art/TechArt/4_Misc/trunk_corpse_prop.tscn')
 
 @onready var VFX_spawn_node = $"../../../../../VFX"
 
@@ -50,6 +50,7 @@ func enter() -> void:
 			child.hide()
 	
 func spawn_explosion():
+		var corpse_prop = trunk_corpse.instantiate()
 		var explosion_VFX = explosion_gpu_emitter.instantiate()
 		var VFX_spawn = $"../../Explosion_target"
 		var VFX_spawn_node = get_tree().current_scene.get_node_or_null("VFX")
@@ -57,13 +58,15 @@ func spawn_explosion():
 			push_warning("No VFX node found in current scene")
 			return
 		explosion_VFX.global_position = VFX_spawn.global_position
+		corpse_prop.global_position = actor.global_position
 		VFX_spawn_node.add_child(explosion_VFX)
+		VFX_spawn_node.add_child(corpse_prop)
 		
 func update(_delta: float) -> void:
 	fade_time += _delta
 	#anim.modulate = Color(1,1,1,lerp(1, 0, fade_time))
 	basic_timer += _delta
 	if basic_timer > max_death_animation_time:
-		if explode_on_kill == true:
+		if leave_trunk_corpse_on_kill == true:
 			spawn_explosion()
 		actor.queue_free()
