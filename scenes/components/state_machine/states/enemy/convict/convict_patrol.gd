c2c2598bcdf7afdf56efdd90d8fb6065a0e70fac
Scazdog <Lscara7@gmail.com>
2026-09-18 14:13:06 +1000

    Stuff i got from the merge

--- scenes/components/state_machine/states/enemy/convict/convict_patrol.gd
+++ scenes/components/state_machine/states/enemy/convict/convict_patrol.gd
@@ -15,6 +15,8 @@ var id: String
 var patrol_timer: float = 4 # time in seconds that enemy walks for
 var patrol_track: float = 0 # timer tracker
 
+var convict_piv
+
 func init(blackboard_dict: Dictionary) -> void:
 	super(blackboard_dict)
 	actor = blackboard["actor"]
@@ -22,6 +24,7 @@ func init(blackboard_dict: Dictionary) -> void:
 	direction = blackboard["direction"]
 	patrol_speed = blackboard["patrol_speed"]
 	stinger_call = blackboard["stinger_call"]
+	convict_piv = blackboard["convict_piv"]
 	id = blackboard["id"]
 	
 
@@ -30,6 +33,7 @@ func update(_delta: float) -> void:
 	if patrol_track >= patrol_timer:
 		actor.velocity = Vector3.ZERO
 		direction *= -1
+		convict_piv.scale.x *= -1
 		patrol_track = 0
 
 func physics_update(_delta: float) -> void:
