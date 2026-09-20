1e9635388ded3a6e37d2e8e6b59aaa09ee550e0e
facelessname <coreychan21@gmail.com>
2026-09-20 23:11:55 +1000

    Added SFX to shotgun and sniper alt fire

--- scenes/components/gun_poc/sniper/beam/beam_component.gd
+++ scenes/components/gun_poc/sniper/beam/beam_component.gd
@@ -88,5 +88,7 @@ func _fire_beam() -> void:
 	var hb = beam.get_node("HitboxComponent")
 	hb.damage_dealt.connect(func(dmg): enemy_hit.emit(dmg))
 	
+	AudioManager.play_sfx("sniper_shoot")
+	
 	sniper._time_since_last_shot = 0.0
 	sniper._handle_ammo()
